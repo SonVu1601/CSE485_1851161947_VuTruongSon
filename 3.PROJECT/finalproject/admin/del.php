78e0458230c19674b9674ecc55bc4db4bb2b7e76
maiphuong16011 <67354983+maiphuong16011@users.noreply.github.com>
2020-11-09 01:10:58 +0700

    Update del.php

--- 3.PROJECT/finalproject/admin/del.php
+++ 3.PROJECT/finalproject/admin/del.php
@@ -1,2 +1,10 @@
+
 <?php
+ require('config.php');
+ $sql="DELETE FROM admin WHERE adminid='".$_GET["adminid"]."'";
+ if(mysqli_query($conn,$sql)){
+     header('location:users.php');
+ }else{
+     echo"Error deleting record:". mysqli_error($conn);
+ }
 ?>
