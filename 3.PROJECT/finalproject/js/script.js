7353c01ebd340cdc53d371215cf9bfb182649b40
maiphuong16011 <67354983+maiphuong16011@users.noreply.github.com>
2020-11-09 22:57:54 +0700

    Update script.js

--- 3.PROJECT/finalproject/js/script.js
+++ 3.PROJECT/finalproject/js/script.js
@@ -4,5 +4,11 @@
               $('.menu').toggle();
             })
           });
+
+<button type="button"
+onclick="document.getElementById('demo').innerHTML = Date()">
+Click me to display Date and Time.</button>
+
+<p id="demo"></p>
           
 
