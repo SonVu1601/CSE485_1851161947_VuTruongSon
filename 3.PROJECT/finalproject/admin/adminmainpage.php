57309830ae2eca55a9c284a2bfe2d49de7792d6d
SonVu1601 <67354093+SonVu1601@users.noreply.github.com>
2020-11-11 00:05:26 +0700

    Update adminmainpage.php

--- 3.PROJECT/finalproject/admin/adminmainpage.php
+++ 3.PROJECT/finalproject/admin/adminmainpage.php
@@ -1,3 +1,6 @@
+<script>
+  alert('Bạn Đã Thành Công');
+</script>
 <?php
 session_start();
 if (!isset($_SESSION['adminUser']))
@@ -23,12 +26,26 @@ if (!isset($_SESSION['adminUser']))
       <img src="logo.png" alt="">
     </div>
     <div class="user-info">
-			<span>Dashboard</span> &nbsp; &nbsp; <a href="#" class="logout-btn">Logout</a>
+    <a href="adminmainpage.php"><span>Dashboard</span></a> <a href="login.php" class="logout-btn">Logout</a>
 		</div>
   </div>
-  <div class="container">
-    <h1>Welcome My Admin</h1>
+  <div class="container dashboard">
+      <h1>Welcome My Admin</h1>
+    <div class="stats">
+			<a href="users.php" class="first">
+				
+				<span>Newly registered users</span>
+			</a>
+			<a href="posts.php">
 
+				<span>Published posts</span>
+			</a>
+		</div>
+		<br><br><br>
+		<div class="buttons">
+			<a href="users.php">Add Users</a>
+			<a href="posts.php">Add Posts</a>
+		</div>
   </div>
   <!-- Optional JavaScript -->
   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
@@ -36,5 +53,3 @@ if (!isset($_SESSION['adminUser']))
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
-
-</html>
