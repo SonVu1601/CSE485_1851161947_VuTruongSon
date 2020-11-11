9e106a63c9def1fed480dc5a1e00b6b09645d0c4
maiphuong16011 <67354983+maiphuong16011@users.noreply.github.com>
2020-11-11 23:37:26 +0700

    Add files via upload

---
+++ 3.PROJECT/finalproject/trangsinhvien.php
@@ -0,0 +1,141 @@
+!doctype html>
+<html lang="en">
+
+<head>
+  <title>Title</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+  <header class="siteheader fixed-top">
+    <div id="menumain">
+      <div class="inner ">
+        <div class="logo ">
+          <a href="http://www.oude.edu.vn/">
+            <img src="img/logo.png" alt="">
+          </a>
+        </div>
+        <nav class="navbar navbar-expand-lg nav-style">
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class=" navbar-nav menu">
+              <li class="nav-item1"><a href=""><i class='fas fa-home current2'></i></a></li>
+              <li class="nav-item dropdown">
+                <a class=" dropdown-toggle nav_content" href="#" id="navbarDropdown" role="button"
+                  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">GIỚI THIỆU</a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a
+                    href="http://oude.edu.vn/introduce/%C4%91%C3%A0o-t%E1%BA%A1o-t%E1%BB%AB-xa-t%E1%BA%A1i-tr%C6%B0%E1%BB%9Dng-%C4%91%E1%BA%A1i-h%E1%BB%8Dc-m%E1%BB%9F-tp-hcm-2/view/">Trung
+                    tâm Đào tạo từ xa</a>
+                  <a
+                    href="http://oude.edu.vn/introduce/m%E1%BA%A1ng-l%C6%B0%E1%BB%9Bi-%C4%91%C3%A0o-t%E1%BA%A1o-r%E1%BB%99ng-kh%E1%BA%AFp-6/view/">Mạng
+                    lưới đào tạo</a>
+                  <a href="http://oude.edu.vn/introduce/li%C3%AAn-k%E1%BA%BFt-qu%E1%BB%91c-t%E1%BA%BF-5/view/">Liên
+                    kết quốc tế</a>
+                  <a href="http://oude.edu.vn/introduce/k%E1%BA%BFt-n%E1%BB%91i-c%E1%BB%99ng-%C4%91%E1%BB%93ng-8/view/">Kết
+                    nối cộng đồng</a>
+                  <a href="http://www.oude.edu.vn/department/index">Các ngành đào tạo</a>
+                  <a href="/page/to-chuc-nhan-su">Tổ chức nhân sự</a>
+                </div>
+              </li>
+              <li class="nav-item dropdown">
+                <a class=" dropdown-toggle nav_content" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                  aria-haspopup="true" aria-expanded="false">SINH VIÊN TƯƠNG LAI</a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a href="/announcement/index">Trang sinh viên</a>
+                  <a href="http://tuyensinh.oude.edu.vn/">Cổng tuyển sinh</a>
+                  <a href="/announcement/index">Thông báo tuyển sinh</a>
+                  <a href="http://www.oude.edu.vn/department/index">Các ngành đào tạo</a>
+                  <a href="/student/mẫu-đơn-68">Các biểu mẫu</a>
+                </div>
+              </li>
+              <li class="nav-item dropdown">
+                <a class=" dropdown-toggle nav_content" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">SINH VIÊN HIỆN TẠI</a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a href="http://lms.oude.edu.vn/">Hệ thống hỗ trợ học tập</a>
+                </div>
+              </li>
+              <li class="nav-item dropdown">
+                <a class=" dropdown-toggle nav_content" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">ĐVLK ĐÀO TẠO</a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a href="/profile/location">Tổng hợp các cơ sở học tập</a>
+                </div>
+              </li>
+              <li class="nav-item dropdown">
+                <a class=" dropdown-toggle nav_content" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">KQ KHẢO SÁT</a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a href="/student/khảo-sát-sinh-viên-mới-tốt-nghiệp-76">Khảo sát sinh viên mới tốt nghiệp</a>
+                </div>
+              </li>
+              
+              <li class="nav-item dropdown">
+                <a class=" dropdown-toggle nav_content" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">LIÊN HỆ</a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a href="tel:18006119">Tổng đài: 18006119</a>
+                </div>
+              </li>
+              
+              
+              <li class="search-item">
+                <span><i class="fa fa-search"></i></span>
+              </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+    <div class="footer2" style="background-color: rgb(45, 45, 45);;">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-6 col-md-8 col-sm-8 col-xs-12 p-left p-right"
+              style="padding-top: 15px;padding-bottom: 15px;">
+              <address>
+                <p style="float: left;">
+                  Copyright © 1990 - 2020 Ho Chi Minh City Open University.
+                  <br>
+                  Room 004, 97 Vo Van Tan street, 6th Ward, 3rd Dist., Ho Chi Minh City, Vietnam - Tel: 18006119
+                  <br>
+                  Website:
+                  <a href="http://www.oude.edu.vn">www.oude.edu.vn</a>
+                  - Email:
+                  <a href="mailto:tuvan@oude.edu.vn">tuvan@oude.edu.vn</a>
+                </p>
+              </address>
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="banner-phone">
+        <a href="tel:18006119"></a>
+      </div>
+  </footer>
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+  </script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+  </script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+  </script>
+  <script src="https://kit.fontawesome.com/ef846d6c04.js" crossorigin="anonymous"></script>
+
+
+
+</body>
+
+</html>
