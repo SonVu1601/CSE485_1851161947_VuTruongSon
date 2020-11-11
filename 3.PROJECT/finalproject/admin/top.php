9affa46a1faa52b55a0a514679a58f4c41e31acc
SonVu1601 <67354093+SonVu1601@users.noreply.github.com>
2020-11-11 23:42:31 +0700

    Update top.php

--- 3.PROJECT/finalproject/admin/top.php
+++ 3.PROJECT/finalproject/admin/top.php
@@ -1,1 +1,28 @@
-<?php?>
+<?php require('config.php');?>
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>Title</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="admin.css">
+  
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+
+<body>
+   
+  <div class="header">
+    <div class="logo">
+      <img src="logo.png" alt="">
+    </div>
+    <div class="user-info">
+			<a href="adminmainpage.php"><span>Dashboard</span></a> &nbsp; &nbsp; <a href="login.php" class="logout-btn">Logout</a>
+		</div>
+  </div>
